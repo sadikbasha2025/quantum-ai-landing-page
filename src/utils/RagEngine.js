f24309e19a7e4bfd6b3b5cd89323fd2beae71cda
Sadik Basha S <sadik.hcl@gmail.com>
2025-11-22 16:33:00 +0530

    Downgrade pdfjs-dist to stable version

--- src/utils/RagEngine.js
+++ src/utils/RagEngine.js
@@ -1,7 +1,7 @@
 import * as pdfjsLib from 'pdfjs-dist';
 
 // Set worker source to CDN to avoid build issues with Vite
-pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`;
 
 export const extractTextFromPDF = async (url) => {
     try {
